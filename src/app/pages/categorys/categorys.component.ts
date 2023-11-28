a4f311d9bbec39ec3688ef892723617b7d7216de
Richard <richard.duque@sectorial.co>
2023-11-27 19:40:39 -0500

    add category

--- src/app/pages/categorys/categorys.component.ts
+++ src/app/pages/categorys/categorys.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CategoryComponent } from './modal/category/category.component';
 
 @Component({
   selector: 'app-categorys',
   templateUrl: './categorys.component.html',
   styleUrls: ['./categorys.component.css']
 })
-export class CategorysComponent {
+export interface IProduct{
+  name:string,
+  date:string,
+  category:[]
+}
 
+export class CategorysComponent {
+  dataCategory!:IProduct
+  constructor(
+    private modalService: NgbModal,
+    private router: Router
+  ) {}
+
+
+  openModal(){
+    const modalRef = this.modalService.open(CategoryComponent, {
+      centered: true,
+      windowClass: 'modal-holder',
+    });
+    modalRef.componentInstance.model = dataCategory;
+    modalRef.componentInstance.onCancel.subscribe(() => modalRef.close());
+    modalRef.componentInstance.onSave.subscribe((user: any) => {
+      modalRef.close();
+      if (user.id) {
+        this.suscriptionsService.editUser(user).subscribe((resp) => {
+          if(resp){
+            this.customMessageAlert.actionMsg('Registro guardado', "OK!!", 'success');
+          }
+        },
+        (error) => {
+          this.customMessageAlert.actionMsg('Ocurrió un error al consultar los usuarios', 'Ups!!', 'error')
+        },
+        () => {
+          this.isBusy = false;
+          this.fetchData();
+        });
+      }
+    });
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { CategorysComponent } from './categorys/categorys.component';
+import { CategoryComponent } from './categorys/modal/category/category.component';
 
 @NgModule({
   declarations: [
-    CategorysComponent
+    CategorysComponent,
+    CategoryComponent
   ],
   imports: [
     CommonModule,
